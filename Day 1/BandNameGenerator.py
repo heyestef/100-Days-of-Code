741b946e6cdc7551657106f3e71c0d3cb273330d
Estefania Ortiz <94660008+heyestef@users.noreply.github.com>
2023-08-22 10:19:02 -0600

    1-Day

---
+++ Day 1/BandNameGenerator.py
@@ -0,0 +1,6 @@
+# Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet_name)
